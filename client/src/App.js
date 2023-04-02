fa2b50550f0694a6f8a99a59573c85bfa28c7528
Edyane <edyanealves@gmail.com>
2023-04-01 22:06:53 -0300

    Importando pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+
+//Pages
+import Home from './pages/Home';
+import Products from './pages/Products';
+import ProductsList from './pages/ProductsList';
+import Search from './pages/Search';
+
 const App = () => {
   return <div>App</div>;
 };
