2e15fb5b95d36b1dc13be9a516017a40ba093f1e
Edyane <edyanealves@gmail.com>
2023-04-02 12:08:29 -0300

    Adicionando routes

--- client/src/App.js
+++ client/src/App.js
@@ -5,15 +5,40 @@ import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 //Pages
 import Home from './pages/Home';
 import Products from './pages/Products';
-import ProductsList from './pages/ProductsList';
+import ProductDetails from './pages/ProductDetails';
 import Search from './pages/Search';
 
 //Components 
 import Header from './components/Header';
 import Footer from './components/Footer';
 
+//Layout 
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+};
+
+const router = createBrowserRouter([
+  { 
+    element: <Layout />,
+    children: [
+      { path: '/', element: <Home />},
+      { path: '/products/:id', element: <Products /> },
+      { path: '/product/:id', element: <ProductDetails /> },
+      { path: '/search', element: <Search /> },
+    ],
+  },
+]);
+
 const App = () => {
-  return <div>App</div>;
+  return <div>
+    <RouterProvider router={router} />
+  </div>;
 };
 
 export default App;
