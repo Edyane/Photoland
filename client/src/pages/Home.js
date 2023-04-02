5f51d7722c98ad32fd6aa53cf3e377fd05ec5255
Edyane <edyanealves@gmail.com>
2023-04-02 15:55:34 -0300

    Importando component 'ProductSlider' na página Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
+//useFetch hook 
+import useFetch from '../hooks/useFetch';
+
+//Import components
+import ProductSlider from '../components/ProductSlider';
+
 const Home = () => {
-  return <div>Home</div>;
+  //Get new projects
+  const { data } = useFetch('/products?populate=*&filters[is*New]=true');
+
+  return (
+    <div className="mb-16">
+      <div className="mx-auto">
+        <h2 className='h2 mb-6 text-center x1:text-left'>Latest Products</h2>
+      </div>
+      <ProductSlider data={data} />
+    </div>
+  )
 };
 
 export default Home;
--- client/src/request.js
+++ client/src/request.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const request = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   headers: {
-    Authorization: 'bearer ' + process.env.REACT_APP_API_KEY,
+    Authorization: 'bearer ' + process.env.REACT_APP_API_TOKEN,
   },
 });
