7f72275f75b65c468bc93e3ed37f7dc374eb5728
Edyane <edyanealves@gmail.com>
2023-04-04 22:03:25 -0300

    Importando Swiper

--- client/src/components/ProductSlider.js
+++ client/src/components/ProductSlider.js
@@ -1,5 +1,15 @@
 import React from 'react';
 
+//Import swiper react components
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+//Import required modules
+import { Pagination, Navigation } from 'swiper';
+
+//Components
+import Product from '../components/Product'
+
 const ProductSlider = ( {data} ) => {
   console.log(data)
   return <div>ProductSlider</div>;
