ff79392a442aee092322167c97f63054c6762986
Edyane <edyanealves@gmail.com>
2023-04-08 13:18:29 -0300

    Construindo component 'ProductSlider'

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Product = ( {product} ) => {
   return (
     <Link>
-      <div className='group'>
+      <div className='grad w-full h-[362px] rounded-[8px] overflow-hidden relative group'>
         {/*image*/}
           <div className="w-full h-[200px] flex items-center justify-center relative">
             <img className="w-[160px] h-[160px] group-hover:scale-90 transition-all" src={`http://localhost:1337${product.attributes.image.data.attributes.url}`} alt="" />
--- client/src/components/ProductSlider.js
+++ client/src/components/ProductSlider.js
@@ -1,21 +1,46 @@
 import React from 'react';
 
 //Import Swiper React Components
-import {Swiper, SwiperSlide} from 'swiper/react';
+import { Swiper, SwiperSlide } from 'swiper/react'
 
 //Import Swiper Styles
 import 'swiper/css';
 import 'swiper/css/pagination';
+import '../slider.css';
 
 //Import Required Modules
 import { Pagination, Navigation } from 'swiper';
-
 //Components
-import Product from '../components/Product'
+import Product from '../components/Product';
 
 const ProductSlider = ({ data }) => {
   return (
-    <Swiper>
+    <Swiper
+    modules={[Pagination, Navigation]} loop={false} navigation={true}
+    breakpoints= {{
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 30
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      1024: {
+        slidesPerView: 2,
+        spaceBetween: 30
+      },
+      1440: {
+        slidesPerView: 3,
+        // slidesPerView: 5,
+        spaceBetween: 30
+      },
+    }}
+    pagination = {{
+      clickable: true,
+    }}
+    className='productSlider mx-auto max-w-[360px] md:max-w-lg x1:max-w-[1410px]'
+    >
       <>
         {data?.map((product) => {
           return (
