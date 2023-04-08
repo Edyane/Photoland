656e16af630e312b1078ef3cf598b98c16d2fc6f
Edyane <edyanealves@gmail.com>
2023-04-07 21:26:47 -0300

    Aplicando estilo a lista de produtos

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 const Product = ( {product} ) => {
   return (
     <Link>
-      <div>
+      <div className='group'>
         {/*image*/}
-          <div>
-            <img src={`http://localhost:1337${product.attributes.image.data.attributes.url}`} alt="" />
+          <div className="w-full h-[200px] flex items-center justify-center relative">
+            <img className="w-[160px] h-[160px] group-hover:scale-90 transition-all" src={`http://localhost:1337${product.attributes.image.data.attributes.url}`} alt="" />
           </div>
       </div>
     </Link>
