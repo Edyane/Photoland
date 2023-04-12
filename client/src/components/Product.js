f2868a4f01c11b7778f7062981982578c3fe4662
Edyane <edyanealves@gmail.com>
2023-04-12 19:34:49 -0300

    Adicionando descrição/valor dos produtos

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -11,6 +11,15 @@ const Product = ( {product} ) => {
           <div className="w-full h-[200px] flex items-center justify-center relative">
             <img className="w-[160px] h-[160px] group-hover:scale-90 transition-all" src={`http://localhost:1337${product.attributes.image.data.attributes.url}`} alt="" />
           </div>
+        {/* text */}
+        <div className="px-6 pb-8 flex flex-col">
+          {/* Category Title */}
+          <div className="text-sm text-accent capitalize mb-2">{product.attributes.categories.data[0].attributes.title}</div>
+          {/* Title */}
+          <div className="text-[15px] mb-4 lg:mb-9">{product.attributes.title.substring(0,35)}...</div>
+          {/* Price */}
+          <div className="text-lg text-accent">{product.attributes.price}</div>
+        </div>
       </div>
     </Link>
 
