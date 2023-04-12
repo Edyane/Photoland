5a4429b4c408c94575b46964bef1eb25e74140c6
Edyane <edyanealves@gmail.com>
2023-04-12 19:21:01 -0300

    Adicionando badge aos produtos

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -5,6 +5,8 @@ const Product = ( {product} ) => {
   return (
     <Link>
       <div className='grad w-full h-[362px] rounded-[8px] overflow-hidden relative group'>
+        {/* badge */}
+        <div className='absolute bg-accent text-primary text-[12px] font-extrabold uppercase top-4 right-4 px-2 rounded-full z-10'>New</div>
         {/*image*/}
           <div className="w-full h-[200px] flex items-center justify-center relative">
             <img className="w-[160px] h-[160px] group-hover:scale-90 transition-all" src={`http://localhost:1337${product.attributes.image.data.attributes.url}`} alt="" />
