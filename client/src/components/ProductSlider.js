5e5716153a4649cd33f57f69a96dc1ac37b69e09
Edyane <edyanealves@gmail.com>
2023-04-21 17:56:41 -0300

    Refatorando slides

--- client/src/components/ProductSlider.js
+++ client/src/components/ProductSlider.js
@@ -11,7 +11,27 @@ import Product from "../components/Product";
 
 const ProductSlider = ({ data }) => {
   return (
-    <Swiper>
+    <Swiper modules={[Pagination, Navigation]} loop={false} navigation={true}
+      breakpoints={{
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 30,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+        },
+        1024: {
+          slidesPerView: 2,
+          spaceBetween: 30,
+        },
+        1440: {
+          slidesPerView: 5,
+          spaceBetween: 30,
+        },
+      }}>
+
+
       <>
         {data?.map((product) => {
           return (
