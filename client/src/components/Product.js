f357319950c3c67d346f2374a0f8f8482ae1901a
Edyane <edyanealves@gmail.com>
2023-04-21 17:48:16 -0300

    Ajustando produtos

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
 
-const Product = () => {
+const Product = ({ product }) => {
+  console.log(product);
   return (
-    <div>Product</div>
-  )
-}
+    <Link>
+      <div className="group">
+    
+        <div className="w-full h-[200px] flex items-center justify-center relative">
+          <img className="w-[160px] h-[160px] group-hover:scale-90 transition-all" src={`http://localhost:1337${product.attributes.image.data.attributes.url}`} alt="" />
+        </div>
+      </div>
+    </Link>
+  );
+};
 
-export default Product
+export default Product;
