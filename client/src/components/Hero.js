7ef1dfe7a5451a4b57bbc43e424b928506ee0fde
Edyane <edyanealves@gmail.com>
2023-04-20 21:18:09 -0300

    Construindo 'Hero' Component

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,7 +1,35 @@
 import React from 'react';
 
+//Components
+import CategoryNavComponent from '../components/CategoryNav';
+import MainSlider from '../components/MainSlider';
+
+//Images
+import PromoImg1 from '../img/promo_img1.png';
+import PromoImg2 from '../img/promo_img2.png'
+import CategoryNav from '../components/CategoryNav';
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section>
+      <div className="container mx-auto">
+        <div>
+          {/* Sidebar */}
+          <div>
+            <CategoryNav />
+          </div>
+          {/* Main Slider */}
+          <div>
+            <MainSlider />
+          </div>
+          {/* Promo imgs */}
+          <div>
+            
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 };
 
 export default Hero;
