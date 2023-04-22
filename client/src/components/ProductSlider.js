81957fd4080b38848ff6ea59d58e9bc5f7fea7b2
Edyane <edyanealves@gmail.com>
2023-04-22 20:30:41 -0300

    Adicionando 'Navigation' ao component 'ProductSlider'

--- client/src/components/ProductSlider.js
+++ client/src/components/ProductSlider.js
@@ -4,6 +4,7 @@ import {Swiper, SwiperSlide} from "swiper/react";
 
 import "swiper/css";
 import "swiper/css/pagination";
+import "swiper/css/navigation";
 import "../slider.css";
 
 import {Pagination, Navigation} from "swiper";
