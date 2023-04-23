7947b47dda3c09e038a4f416c6283cc054c2cd07
Edyane <edyanealves@gmail.com>
2023-04-22 21:09:48 -0300

    Ajustando classes de Tailwind no component 'LatestProducts'

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from 'react';
+import CategoryNav from "../components/CategoryNav";
+import MainSlider from '../components/MainSlider';
+
+import PromoImg1 from "../img/promo_img1.png";
+import PromoImg2 from "../img/promo_img2.png";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="mb-[30px] pt-36 lg:pt-0">
+      <div className="container mx-auto">
+        <div className="flex flex-col gap-y-[30px] xl:flex-row xl:gap-x-[30px]">
+
+          <div>
+            <CategoryNav />
+          </div>
+
+          <div className="w-full max-w-lg bg-pink-200">
+            <MainSlider />
+          </div>
+
+          <div>
+            <div>Promo 1</div>
+            <div>Promo 2</div>
+          </div>
+
+        </div>
+      </div>
+    </section>
   )
 }
 
-export default Hero
+export default Hero;
--- client/src/components/LatestProducts.js
+++ client/src/components/LatestProducts.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import useFetch from '../hooks/useFetch';
+import ProductSlider from "../components/ProductSlider";
 
 const LatestProducts = () => {
+  //Get new products
+  const { data } = useFetch("/products?populate=*&filters[isNew]=true");
+  console.log(data);
+
   return (
-    <div>LatestProducts</div>
+    <div className="mb-16">
+      <div className="container mx-auto">
+        <h2 className="h2 mb-6 text-center x1:text-left">Latest Products</h2>
+      </div>
+      <ProductSlider data={data} />
+    </div>
   )
 }
 
-export default LatestProducts
+export default LatestProducts;
