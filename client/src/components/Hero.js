d46f8aa60da67435bd6c9e70c844f22fb3693700
Edyane <edyanealves@gmail.com>
2023-04-24 20:50:04 -0300

    Adicionando promos ao component 'Hero'

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -19,9 +19,45 @@ const Hero = () => {
             <MainSlider />
           </div>
 
-          <div classname="flex flex-col gap-y-[30px] w-full max-w-lg mx-auto h-[500px] bg-pink-50/10">
-            <div>Promo 1</div>
-            <div>Promo 2</div>
+          {/* Promos */}
+          <div classname="flex flex-col gap-y-[30px] w-full max-w-lg mx-auto h-[500px]">
+
+            {/* Promo 1 */}
+            <div className="grad flex-1 h-[250px] rounded-[8px] overflow-hidden relative p-6">
+              {/* Text */}
+              <div className="flex flex-col max-w-[144px] h-full justify-center">
+
+                <div className="text-[20px] uppercase font-medium leading-tight mb-4">
+                  Save 35% all dskr cameras
+                </div>
+
+                <a href="#" className="uppercase text-accent">
+                  Shop now
+                </a>
+
+              </div>
+
+              <img className="absolute z-20 -top-2 -right-4" src={PromoImg1} alt="" />
+            </div>
+
+            {/* Promo 2 */}
+            <div className="grad flex-1 h-[250px] rounded-[8px] overflow-hidden relative p-6">
+              {/* Text */}
+              <div className="flex flex-col max-w-[144px] h-full justify-center">
+
+                <div className="text-[20px] uppercase font-medium leading-tight mb-4">
+                  Save 35% all dskr cameras
+                </div>
+
+                <a href="#" className="uppercase text-accent">
+                  Shop now
+                </a>
+
+              </div>
+
+              <img className="absolute z-20 -top-2 -right-4" src={PromoImg1} alt="" />
+            </div>
+
           </div>
 
         </div>
