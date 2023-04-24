f711645c928a52e60bb735bb274767b01826d9ee
Edyane <edyanealves@gmail.com>
2023-04-24 20:59:40 -0300

    Ajustando código do component 'Hero'

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,69 +1,61 @@
-import React from 'react';
+import React from "react";
 import CategoryNav from "../components/CategoryNav";
-import MainSlider from '../components/MainSlider';
+import MainSlider from "../components/MainSlider";
 
 import PromoImg1 from "../img/promo_img1.png";
 import PromoImg2 from "../img/promo_img2.png";
 
 const Hero = () => {
-  return (
-    <section className="mb-[30px] pt-36 lg:pt-0">
-      <div className="container mx-auto">
-        <div className="flex flex-col gap-y-[30px] xl:flex-row xl:gap-x-[30px]">
+	return (
+		<section className="mb-[30px] pt-36 lg:pt-0">
+			<div className="container mx-auto">
+				<div className="flex flex-col gap-y-[30px] xl:flex-row xl:gap-x-[30px]">
+					<div>
+						<CategoryNav />
+					</div>
 
-          <div>
-            <CategoryNav />
-          </div>
+					<div className="w-full max-w-lg lg:max-w-[734px] mx-auto">
+						<MainSlider />
+					</div>
 
-          <div className="w-full max-w-lg lg:max-w-[734px] mx-auto">
-            <MainSlider />
-          </div>
+					{/* Promos */}
+					<div className="flex flex-col gay-y-[30px] w-full max-w-lg mx-auto h-[500px] bg-pink-50/10">
+						{/* Promo 1 */}
+						<div className="grad flex-1 h-[250px] rounded-[8px] overflow-hidden relative p-6">
+							{/* Text */}
+							<div className="flex flex-col max-w-[144px] h-full justify-center">
+								<div className="text-[20px] uppercase font-medium leading-tight mb-4">
+									Save 35% all dskr cameras
+								</div>
 
-          {/* Promos */}
-          <div classname="flex flex-col gap-y-[30px] w-full max-w-lg mx-auto h-[500px]">
+								<a href="#" className="uppercase text-accent">
+									Shop now
+								</a>
+							</div>
 
-            {/* Promo 1 */}
-            <div className="grad flex-1 h-[250px] rounded-[8px] overflow-hidden relative p-6">
-              {/* Text */}
-              <div className="flex flex-col max-w-[144px] h-full justify-center">
+							<img className="absolute z-20 -top-2 -right-4" src={PromoImg1} alt="" />
+						</div>
 
-                <div className="text-[20px] uppercase font-medium leading-tight mb-4">
-                  Save 35% all dskr cameras
-                </div>
+						{/* Promo 2 */}
+						<div className="grad flex-1 h-[250px] rounded-[8px] overflow-hidden relative p-6">
+							{/* Text */}
+							<div className="flex flex-col max-w-[144px] h-full justify-center">
+								<div className="text-[20px] uppercase font-medium leading-tight mb-4">
+									Save 35% all dskr cameras
+								</div>
 
-                <a href="#" className="uppercase text-accent">
-                  Shop now
-                </a>
+								<a href="#" className="uppercase text-accent">
+									Shop now
+								</a>
+							</div>
 
-              </div>
-
-              <img className="absolute z-20 -top-2 -right-4" src={PromoImg1} alt="" />
-            </div>
-
-            {/* Promo 2 */}
-            <div className="grad flex-1 h-[250px] rounded-[8px] overflow-hidden relative p-6">
-              {/* Text */}
-              <div className="flex flex-col max-w-[144px] h-full justify-center">
-
-                <div className="text-[20px] uppercase font-medium leading-tight mb-4">
-                  Save 35% all dskr cameras
-                </div>
-
-                <a href="#" className="uppercase text-accent">
-                  Shop now
-                </a>
-
-              </div>
-
-              <img className="absolute z-20 -top-2 -right-4" src={PromoImg1} alt="" />
-            </div>
-
-          </div>
-
-        </div>
-      </div>
-    </section>
-  )
-}
+							<img className="absolute z-20 -top-2 -right-4" src={PromoImg1} alt="" />
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
 
 export default Hero;
