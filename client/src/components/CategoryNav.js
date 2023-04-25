cbaadde129444aae7a6dbe8f241cde6295b806dd
Edyane <edyanealves@gmail.com>
2023-04-25 20:26:03 -0300

    Construindo 'CategoryNav' component

--- client/src/components/CategoryNav.js
+++ client/src/components/CategoryNav.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React from "react";
 import useFetch from "../hooks/useFetch.js";
 import { Link } from "react-router-dom";
 
 const CategoryNav = () => {
-  const { data } = useFetch('/categories');
-  console.log(data);
+	const { data } = useFetch("/categories");
 
-  return (
-    <div>
-      Category Nav
-    </div>
-  )
+	return (
+		<aside className="hidden xl:flex">
+			<div className="bg-primary flex flex-col w-[286px] h-[500px] rounded-[8px] overflow-hidden">
+				<div className="bg-accent py-4 text-primary uppercase font-semibold flex items-center justify-center">Browse Categories</div>
+				<div className="flex flex-col gap-y-6 p-6">
+					{data?.map(category => {
+						console.log(category);
+						return <Link to={`/products/${category.id}`} className="cursor-pointer uppercase" key={category.id}>{category.attributes.title}</Link>
+					})}
+				</div>
+			</div>
+		</aside>
+	)
 };
 
 export default CategoryNav;
