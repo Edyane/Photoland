c3ee5a7b17a0cd19996424633ce2796ebb3aab33
Edyane <edyanealves@gmail.com>
2023-04-26 22:04:34 -0300

    Construindo 'MainSlider' component

--- client/src/components/MainSlider.js
+++ client/src/components/MainSlider.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React from "react";
 
-const MainSlider = () => {
-  return <div>MainSlider</div>;
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "../slider.css";
+
+import { Pagination } from "swiper";
+import CameraImg from "../img/camera.png";
+
+const MainSlider = ({ data }) => {
+	return (
+		<Swiper
+			modules={[Pagination]}
+			loop={true}
+			pagination={{
+				clickable: true,
+			}}
+			className="mainSlider mx-auto max-w-[360px] md:max-w-lg xl:max-w-[1410px]"
+		>
+			<>
+				<SwiperSlide>slide</SwiperSlide>
+			</>
+		</Swiper>
+	);
 };
 
 export default MainSlider;
