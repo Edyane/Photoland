e940bcaafa28c7ab21a93070048a9f018b360043
Edyane <edyanealves@gmail.com>
2023-04-28 20:18:54 -0300

    Importando components

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,6 +7,11 @@ import { FiMenu} from "react-icons/fi";
 
 import {Link} from "react-router-dom";
 
+import SearchForm from "../components/SearchForm";
+import CategoryNavMobile from "../components/CategoryNavMobile";
+import Cart from "../components/Cart";
+
+
 const Header = () => {
   return <div>Header</div>;
 };
