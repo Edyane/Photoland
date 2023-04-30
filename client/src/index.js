b595b802cbba1a66e4289e422436f1435baa7464
Edyane <edyanealves@gmail.com>
2023-04-30 20:10:36 -0300

    Adicionando 'CartProvider' ao index.js

--- client/src/index.js
+++ client/src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+import CartProvider from "./context/CartContext";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<App />
+		<CartProvider>
+			<App />
+		</CartProvider>
 	</React.StrictMode>
 );
