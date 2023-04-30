c71f6583ff19915031d68a3d2370e2115ac777d8
Edyane <edyanealves@gmail.com>
2023-04-29 21:00:51 -0300

    Adicionando icons ao component 'Header'

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,19 +1,46 @@
-import React, {useState} from 'react';
-
-import Logo from "../img/logo.png";
+import React, { useState } from 'react';
+import Logo from "../img/logo.png"
 
 import { SlBag } from "react-icons/sl";
-import { FiMenu} from "react-icons/fi";
+import { FiMenu } from "react-icons/fi"
 
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 import SearchForm from "../components/SearchForm";
 import CategoryNavMobile from "../components/CategoryNavMobile";
 import Cart from "../components/Cart";
 
-
 const Header = () => {
-  return <div>Header</div>;
-};
+  return (
+    <header>
+      <div className="container mx-auto">
+        <div>
 
-export default Header;
+          <div>
+            <FiMenu />
+          </div>
+
+          <div>
+            <CategoryNavMobile />
+          </div>
+
+          <Link>
+            <img src={Logo} alt='' />
+          </Link>
+
+          <div className="hidden w-full x1:flex x1:max-w-[734px]">
+            <SearchForm />
+          </div>
+         
+
+        </div>
+
+        <div className="lg:hidden">
+          <SearchForm />
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
