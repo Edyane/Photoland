8ed6dd2909ca7193bbd68a81a5954098112a4455
Edyane <edyanealves@gmail.com>
2023-05-03 20:21:09 -0300

    Corrigindo bug e ajustande classes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -13,51 +13,53 @@ import { CartContext } from "../context/CartContext";
 
 const Header = () => {
 	const { isOpen, setIsOpen } = useContext(CartContext);
-	<header className="bg-primary py-6 fixed w-full top-0 z-40 lg:relative x1:mb-[30px]">
-		<div className="container mx-auto">
-			<div className="flex flex-row gap-4 lg:items-center justify-between mb-4 lg:mb-0">
-				<div>
-					<FiMenu />
+	return (
+		<header className="bg-primary py-6 fixed w-full top-0 z-40 lg:relative xl:mb-[30px]">
+			<div className="container mx-auto">
+				<div className="flex flex-row gap-4 lg:items-center justify-between mb-4 lg:mb-0">
+					<div>
+						<FiMenu />
+					</div>
+
+					<div>
+						<CategoryNavMobile />
+					</div>
+
+					<Link>
+						<img src={Logo} alt="" />
+					</Link>
+
+					<div className="hidden w-full xl:flex xl:max-w-[734px]">
+						<SearchForm />
+					</div>
+
+					<div>
+						<div>Need help? 123 456 789</div>
+
+						<div
+							onClick={() => setIsOpen(!isOpen)}
+							className="relative cursor-pointer"
+						>
+							<SlBag className="text-2x1" />
+							<div>2</div>
+						</div>
+
+						<div
+							className={`
+							${isOpen ? "right-0" : "-right-full"}
+							bg-[#0e0f10] shadow-xl fixed top-0 bottom-0 w-full z-10 md:max-w-[500px] transition-all duration-300`}
+						>
+							<Cart />
+						</div>
+					</div>
 				</div>
 
-				<div>
-					<CategoryNavMobile />
-				</div>
-
-				<Link>
-					<img src={Logo} alt="" />
-				</Link>
-
-				<div className="hidden w-full x1:flex x1:max-w-[734px]">
+				<div className="lg:hidden">
 					<SearchForm />
 				</div>
-
-				<div>
-					<div>Need help? 123 456 789</div>
-
-					<div
-						onClick={() => setIsOpen(!isOpen)}
-						className="relative cursor-pointer"
-					>
-						<SlBag className="text-2x1" />
-						<div>2</div>
-					</div>
-
-					<div
-						className={`
-							${isOpen ? "right-0" : "-right-full"}
-							bg-[#0e0f10] shadow-x1 fixed top-0 bottom-0 w-full z-10 md:max-w-[500px] transition-all duration-300`}
-					>
-						<Cart />
-					</div>
-				</div>
 			</div>
-
-			<div className="lg:hidden">
-				<SearchForm />
-			</div>
-		</div>
-	</header>;
+		</header>
+	);
 };
 
 export default Header;
