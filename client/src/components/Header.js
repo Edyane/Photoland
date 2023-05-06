886a6a3a42c58c0ea2a29db196651742e782e5a1
Edyane <edyanealves@gmail.com>
2023-05-05 23:09:12 -0300

    Adicionando evento ao component 'FiMenu'

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -18,12 +18,12 @@ const Header = () => {
 		<header className="bg-primary py-6 fixed w-full top-0 z-40 lg:relative xl:mb-[30px]">
 			<div className="container mx-auto">
 				<div className="flex flex-row gap-4 lg:items-center justify-between mb-4 lg:mb-0">
-					<div className="text-3x1 xl:hidden cursor-pointer">
+					<div onClick={() => setCatnavMobile(true)} className="text-3x1 xl:hidden cursor-pointer">
 						<FiMenu />
 					</div>
 
 					<div className={`${ catNavMobile ? 'left-0' : '-left-full'} fixed top-0 bottom-0 z-30 w-full h-screen transition-all duration-200`}>
-						<CategoryNavMobile />
+						<CategoryNavMobile setCatnavMobile={setCatnavMobile} />
 					</div>
 
 					<Link>
