6855e61f987609c252d8ce2ccee7ff5ba11be674
Edyane <edyanealves@gmail.com>
2023-05-07 09:51:28 -0300

    Corrigindo bugs em 'CategoryNavMobile'

--- client/src/components/CategoryNavMobile.js
+++ client/src/components/CategoryNavMobile.js
@@ -1,13 +1,16 @@
 import React from "react";
-import {FiX} from "react-icons/f1";
+import {FiX} from "react-icons/fi";
 
 const CategoryNavMobile = ({ setCatnavMobile }) => {
 	return (
 		<div className="w-full h-full bg-primary p-8">
-			<div className="flex justify-end mb-8 cursor-pointer">
+
+			<div onClick={() => setCatnavMobile(false)}
+			className="flex justify-end mb-8 cursor-pointer">
                 <FiX className="text-3x1" />
             </div>
 			CategoryNavMobile
+
 		</div>
 	);
 };
