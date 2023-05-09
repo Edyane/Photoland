d6ce9681cb2635b396f2c16b62495a9e8241532a
Edyane <edyanealves@gmail.com>
2023-05-09 19:37:52 -0300

    Ajustando classes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -17,7 +17,7 @@ const Header = () => {
 	return (
 		<header className="bg-primary py-6 fixed w-full top-0 z-40 lg:relative xl:mb-[30px]">
 			<div className="container mx-auto">
-				<div className="flex flex-row gap-4 lg:items-center justify-between mb-4 lg:mb-0">
+				<div className="flex flex-row gap-4 lg:items-center justify-between mb-4 xl:mb-0">
 					<div onClick={() => setCatnavMobile(true)} className="text-3x1 xl:hidden cursor-pointer">
 						<FiMenu />
 					</div>
@@ -55,7 +55,7 @@ const Header = () => {
 					</div>
 				</div>
 
-				<div className="lg:hidden">
+				<div className="xl:hidden">
 					<SearchForm />
 				</div>
 			</div>
