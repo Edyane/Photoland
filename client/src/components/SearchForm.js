70a48cb5ca018f005e8e1aac5e7f8500c0339657
Edyane <edyanealves@gmail.com>
2023-05-10 22:48:08 -0300

    Construindo barra de busca

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -7,9 +7,15 @@ const SearchForm = () => {
   const navigate = useNavigate();
   const {searchTerm, setSearchTerm} = useNavigate('');
 
+  const handleSearchInput = (e) => {
+    console.log(e.target.value);
+  }
+
   return (
     <form className=" w-full relative">
-      <input className="input" type="text" placeholder="Search for a product..." />
+      <input 
+      onChange= {handleSearchInput}
+      className="input" type="text" placeholder="Search for a product..." />
 
       <button className="btn btn-accent absolute top-0 right-0 rounded-tl-none rounded-bl-none">
         <FiSearch className="text-xl" />
