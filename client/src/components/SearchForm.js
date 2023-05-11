0b11ce8f94f30b4ae9df5c63e4cff551e0636074
Edyane <edyanealves@gmail.com>
2023-05-10 22:48:46 -0300

    Ajustando espaçamento do código

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -1,27 +1,30 @@
-import React from 'react';
+import React from "react";
 
 import { FiSearch } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
 
 const SearchForm = () => {
-  const navigate = useNavigate();
-  const {searchTerm, setSearchTerm} = useNavigate('');
+	const navigate = useNavigate();
+	const { searchTerm, setSearchTerm } = useNavigate("");
 
-  const handleSearchInput = (e) => {
-    console.log(e.target.value);
-  }
+	const handleSearchInput = (e) => {
+		console.log(e.target.value);
+	};
 
-  return (
-    <form className=" w-full relative">
-      <input 
-      onChange= {handleSearchInput}
-      className="input" type="text" placeholder="Search for a product..." />
+	return (
+		<form className=" w-full relative">
+			<input
+				onChange={handleSearchInput}
+				className="input"
+				type="text"
+				placeholder="Search for a product..."
+			/>
 
-      <button className="btn btn-accent absolute top-0 right-0 rounded-tl-none rounded-bl-none">
-        <FiSearch className="text-xl" />
-      </button>
-    </form>
-  );
+			<button className="btn btn-accent absolute top-0 right-0 rounded-tl-none rounded-bl-none">
+				<FiSearch className="text-xl" />
+			</button>
+		</form>
+	);
 };
 
 export default SearchForm;
