86077895edf4b852d961253be6eaa65f5461bb94
Edyane <edyanealves@gmail.com>
2023-05-11 21:15:34 -0300

    Limpando console da página

--- client/src/components/CategoryNav.js
+++ client/src/components/CategoryNav.js
@@ -11,7 +11,6 @@ const CategoryNav = () => {
 				<div className="bg-accent py-4 text-primary uppercase font-semibold flex items-center justify-center">Browse Categories</div>
 				<div className="flex flex-col gap-y-6 p-6">
 					{data?.map(category => {
-						console.log(category);
 						return <Link to={`/products/${category.id}`} className="cursor-pointer uppercase" key={category.id}>{category.attributes.title}</Link>
 					})}
 				</div>
