350890c45912965e45b2d373ac05d822f95f3d4f
Edyane <edyanealves@gmail.com>
2023-05-14 18:37:18 -0300

    Feat: ajustando construção do component 'Search'

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React from "react";
+import { useLocation } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+import CategoryNav from "../components/CategoryNav";
+import Product from "../components/Product";
 
 const Search = () => {
-  return <div>Search</div>;
+	const location = useLocation();
+	const searchParams = new URLSearchParams(location.search);
+	const searchTerm = searchParams.get('query');
+	console.log(searchTerm);
+
+	const {data} = useFetch(`/products?populate=*&filters[title][$contains]=${searchTerm}`);
+	console.log(data);
+
+	return (
+		<div className="mb-[30px] pt-40 lg:pt-4 xl:pt-0">
+			<div className="container mx-auto">
+				<div className="flex gap-x-[30px]">	
+					<CategoryNav />				
+					
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Search;
