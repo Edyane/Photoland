8b7855c56d6d8857816c007f40bde6aae42c3c24
Edyane <edyanealves@gmail.com>
2023-05-14 18:36:32 -0300

    Feat: adicionando funcionalidades ao component 'SearchForm'

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -23,7 +23,6 @@ const SearchForm = () => {
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log(searchTerm);
 		if (searchTerm.length > 0) {
 			navigate(`/search?query=${searchTerm}`);
 			document.querySelector("input").value = '';
