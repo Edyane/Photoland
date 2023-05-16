1deccedb8926d88e0e80df43d7ed5e56094e09ec
Edyane <edyanealves@gmail.com>
2023-05-15 21:30:17 -0300

    Feat: consumindo api através do hook useFetch

--- client/src/components/CategoryNavMobile.js
+++ client/src/components/CategoryNavMobile.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { FiX } from "react-icons/fi";
 
+// import { Link } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+
 const CategoryNavMobile = ({ setCatnavMobile }) => {
+	//get categories
+	const {data} = useFetch('/categories');
+	console.log(data)
+	
 	return (
 		<div className="w-full h-full bg-primary p-8">
 
@@ -9,6 +16,8 @@ const CategoryNavMobile = ({ setCatnavMobile }) => {
 			className="flex justify-end mb-8 cursor-pointer">
                 <FiX className="text-3x1" />
             </div>
+
+			<div></div>
 			CategoryNavMobile
 
 		</div>
