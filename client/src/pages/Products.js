9e1fbfe81b420a92453d53a3af2a4dca310f4aa4
Edyane <edyanealves@gmail.com>
2023-05-16 21:40:39 -0300

    Fix: corrigindo bug de importação

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-// import useFetch from "../hooks/useFetch";
-import CategoryNav from "../components/Product";
-// import Product from "../components/Product";
+import useFetch from "../hooks/useFetch";
+import CategoryNav from "../components/CategoryNav";
+import Product from "../components/Product";
 
 const Products = () => {
-	const { id } = useParams;
+	const { id } = useParams();
 	console.log(id);
 	return (
 		<div className="mb-16 pt-40 lg:pt-0">
