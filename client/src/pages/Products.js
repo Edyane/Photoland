2e81416e3a76e7d0cf4a96eef9f99c6fbf27aa90
Edyane <edyanealves@gmail.com>
2023-05-16 21:10:01 -0300

    Feat: construindo página de produtos

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+
+import useFetch from '../hooks/useFetch';
+import CategoryNav from '../components/Product';
 
 const Products = () => {
-  return <div>Products</div>;
+  const {id} = useParams();
+  console.log(id);
+  return <div className="mb-16 pt-40 lg:pt-0">
+    <div className="container mx-auto">
+      <div>
+        <CategoryNav />
+        <main>
+
+          <div>title</div>
+
+          <div>
+            product grid
+          </div>
+        </main>
+      </div>
+    </div>
+  </div>;
 };
 
 export default Products;
