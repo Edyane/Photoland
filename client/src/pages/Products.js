0fb5e32af6b952db7548c058a71e44bad753f4f9
Edyane <edyanealves@gmail.com>
2023-05-16 21:10:46 -0300

    Style: ajustando código

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -1,27 +1,26 @@
-import React, {useState, useEffect} from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-import useFetch from '../hooks/useFetch';
-import CategoryNav from '../components/Product';
+import useFetch from "../hooks/useFetch";
+import CategoryNav from "../components/Product";
 
 const Products = () => {
-  const {id} = useParams();
-  console.log(id);
-  return <div className="mb-16 pt-40 lg:pt-0">
-    <div className="container mx-auto">
-      <div>
-        <CategoryNav />
-        <main>
+	const { id } = useParams();
+	console.log(id);
+	return (
+		<div className="mb-16 pt-40 lg:pt-0">
+			<div className="container mx-auto">
+				<div>
+					<CategoryNav />
+					<main>
+						<div>title</div>
 
-          <div>title</div>
-
-          <div>
-            product grid
-          </div>
-        </main>
-      </div>
-    </div>
-  </div>;
+						<div>product grid</div>
+					</main>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Products;
