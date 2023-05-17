514af91ff0d8c2a0c76b456c8b7192a605db01f4
Edyane <edyanealves@gmail.com>
2023-05-16 21:26:39 -0300

    Style: ajustando código

--- client/src/components/CategoryNavMobile.js
+++ client/src/components/CategoryNavMobile.js
@@ -6,20 +6,29 @@ import useFetch from "../hooks/useFetch";
 
 const CategoryNavMobile = ({ setCatnavMobile }) => {
 	//get categories
-	const {data} = useFetch('/categories');
-	console.log(data)
-	
+	const { data } = useFetch("/categories");
+	console.log(data);
+
 	return (
 		<div className="w-full h-full bg-primary p-8">
-
-			<div onClick={() => setCatnavMobile(false)}
-			className="flex justify-end mb-8 cursor-pointer">
-                <FiX className="text-3x1" />
-            </div>
+			<div
+				onClick={() => setCatnavMobile(false)}
+				className="flex justify-end mb-8 cursor-pointer"
+			>
+				<FiX className="text-3x1" />
+			</div>
 
 			<div className="flex flex-col gap-y-8">
 				{data?.map((category) => {
-					return <Link to={`products/${category.id}`}className="uppercase font-medium" key={category.id}>{category.attributes.title}</Link>
+					return (
+						<Link
+							to={`products/${category.id}`}
+							className="uppercase font-medium"
+							key={category.id}
+						>
+							{category.attributes.title} Cameras
+						</Link>
+					);
 				})}
 			</div>
 		</div>
