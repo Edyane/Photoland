de2956542fea18ef7d1bbe0e558e76a2bca5acf4
Edyane <edyanealves@gmail.com>
2023-05-17 22:11:57 -0300

    Fix: corrigindo bugs

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -9,14 +9,23 @@ const Products = () => {
 	const { id } = useParams();
 
 	//get products based on category id
-	const {data} = useFetch(`/products?populate=*&filters[categories][id][$eq]=${id}`);
+	const { data } = useFetch(`/products?populate=*&filters[categories][id][$eq]=${id}`);
+	const [title, setTitle] = useState(null);
+
+	useEffect(() => {
+		if (data) {
+			setTitle(data[0].attributes.categories.data[0].attributes.title);
+		}
+	}, [data]);
+
+
 	return (
 		<div className="mb-16 pt-40 lg:pt-0">
 			<div className="container mx-auto">
 				<div className="flex gap-x-[30px]">
 					<CategoryNav />
 					<main>
-						<div>title</div>
+						<div className="py-3 text-xl uppercase text-center lg:text-left">{title} cameras</div>
 
 						<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-[15px] md:gap-[30px]">
 							{data?.map(product => {
