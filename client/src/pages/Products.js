230f98513afbacdfba7018357fa01080ee2b9d0a
Edyane <edyanealves@gmail.com>
2023-05-17 22:12:45 -0300

    Style: ajustando código

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -9,7 +9,9 @@ const Products = () => {
 	const { id } = useParams();
 
 	//get products based on category id
-	const { data } = useFetch(`/products?populate=*&filters[categories][id][$eq]=${id}`);
+	const { data } = useFetch(
+		`/products?populate=*&filters[categories][id][$eq]=${id}`
+	);
 	const [title, setTitle] = useState(null);
 
 	useEffect(() => {
@@ -18,18 +20,24 @@ const Products = () => {
 		}
 	}, [data]);
 
-
 	return (
 		<div className="mb-16 pt-40 lg:pt-0">
 			<div className="container mx-auto">
 				<div className="flex gap-x-[30px]">
 					<CategoryNav />
 					<main>
-						<div className="py-3 text-xl uppercase text-center lg:text-left">{title} cameras</div>
+						<div className="py-3 text-xl uppercase text-center lg:text-left">
+							{title} cameras
+						</div>
 
 						<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-4 gap-[15px] md:gap-[30px]">
-							{data?.map(product => {
-							return <Product product={product} key={product.id} />
+							{data?.map((product) => {
+								return (
+									<Product
+										product={product}
+										key={product.id}
+									/>
+								);
 							})}
 						</div>
 					</main>
