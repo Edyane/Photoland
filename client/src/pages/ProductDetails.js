9a30044893e1d1217c41901042db9a5123035413
Edyane <edyanealves@gmail.com>
2023-05-18 21:46:14 -0300

    Style: ajustando código

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import { useParams } from "react-router-dom";
 
 import useFetch from "../hooks/useFetch";
@@ -8,12 +8,14 @@ import RelatedProducts from "../components/RelatedProducts";
 import { CartContext } from "../context/CartContext";
 
 const ProductDetails = () => {
-  const {id} = useParams();
-  console.log(id);
-  //get product data base on the id
-  const { data } = useFetch(`/products?populate=*&filters[id][$eq]=${id}`);
-  console.log(data);
-  return <div className="mb-16 pt-44 lg:pt-[30px] xl:pt-0">ProductDetails</div>;
+	const { id } = useParams();
+	console.log(id);
+	//get product data base on the id
+	const { data } = useFetch(`/products?populate=*&filters[id][$eq]=${id}`);
+	console.log(data);
+	return (
+		<div className="mb-16 pt-44 lg:pt-[30px] xl:pt-0">ProductDetails</div>
+	);
 };
 
 export default ProductDetails;
