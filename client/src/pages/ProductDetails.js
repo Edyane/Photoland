f4cbcd1e43bc5f09a9b45ac0d50d960908652b96
Edyane <edyanealves@gmail.com>
2023-05-18 21:36:09 -0300

    Feat: adicionando importações

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import { useParams } from "react-router-dom";
+
+import useFetch from "../hooks/useFetch";
+
+import RelatedProducts from "../components/RelatedProducts";
+
+import { CartContext } from "../context/CartContext";
 
 const ProductDetails = () => {
   return <div>ProductDetails</div>;
