f26fab28c932da4a0ddd9051456e95595729521b
Edyane <edyanealves@gmail.com>
2023-05-18 21:45:21 -0300

    Feat: construindo detalhamento de produtos

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -8,7 +8,12 @@ import RelatedProducts from "../components/RelatedProducts";
 import { CartContext } from "../context/CartContext";
 
 const ProductDetails = () => {
-  return <div>ProductDetails</div>;
+  const {id} = useParams();
+  console.log(id);
+  //get product data base on the id
+  const { data } = useFetch(`/products?populate=*&filters[id][$eq]=${id}`);
+  console.log(data);
+  return <div className="mb-16 pt-44 lg:pt-[30px] xl:pt-0">ProductDetails</div>;
 };
 
 export default ProductDetails;
