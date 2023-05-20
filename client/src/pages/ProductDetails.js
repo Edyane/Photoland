e3e4a6313eb8dc9e14baed3967d1d911eda1c7a5
Edyane <edyanealves@gmail.com>
2023-05-19 21:43:01 -0300

    Feat: adicionando títulos'

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -12,14 +12,20 @@ const ProductDetails = () => {
 	console.log(id);
 	//get product data base on the id
 	const { data } = useFetch(`/products?populate=*&filters[id][$eq]=${id}`);
-	console.log(data);
+	if(!data) {
+		return <div className="container mx-auto">Loading...</div>;
+	}
 	return (
 		<div className="mb-16 pt-44 lg:pt-[30px] xl:pt-0">
 			<div className="container mx-auto">
 				<div>
 					<div>
-						<img src={data[0].attributes.image.data.attributes.url} alt=""/>
+						<img src={`http://localhost:1337${data[0].attributes.image.data.attributes.url}`} alt="" className="w-full max-w-[65%]"/>
 					</div>
+				</div>
+
+				<div>
+					<div>{data[0].attributes.categories.data[0].attributes.title}</div>
 				</div>
 				<RelatedProducts />
 			</div>
