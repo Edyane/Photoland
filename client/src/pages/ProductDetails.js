505a6a8396f69591cd9359e0f69d6ffe3b845533
Edyane <edyanealves@gmail.com>
2023-05-21 21:20:42 -0300

    Style: ajustando código

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -18,36 +18,42 @@ const ProductDetails = () => {
 	return (
 		<div className="mb-16 pt-44 lg:pt-[30px] xl:pt-0">
 			<div className="container mx-auto">
-
 				<div className="flex flex-col lg:flex-row gap-[30px] mb-[30px]">
 					<div className="flex-1 lg:max-w-[40%] lg:h-[540px] grad rounded-lg flex justify-center items center">
-						<img src={`http://localhost:1337${data[0].attributes.image.data.attributes.url}`}
+						<img
+							src={`http://localhost:1337${data[0].attributes.image.data.attributes.url}`}
 							alt=""
-							className="w-full max-w-[65%]"/>
+							className="w-full max-w-[65%]"
+						/>
 					</div>
-					
+
 					<div className="flex-1 bg-primary p-12 xl:p-20 rounded-lg flex flex-col justify-center">
 						<div className="uppercase text-accent text-lg font-medium mb-2">
-							{data[0].attributes.categories.data[0].attributes.title}{" "}
+							{
+								data[0].attributes.categories.data[0].attributes
+									.title
+							}{" "}
 							cameras
 						</div>
 
 						<h2 className="h2 mb-4">{data[0].attributes.title}</h2>
 
-						<p className="mb-12">{data[0].attributes.description}</p>
+						<p className="mb-12">
+							{data[0].attributes.description}
+						</p>
 
 						<div className="flex items-center gap-x-8">
-							<div className="text-3xl text-accent font-semibold">${data[0].attributes.price}</div>
-							<button className="btn btn-accent">Add to cart</button>
+							<div className="text-3xl text-accent font-semibold">
+								${data[0].attributes.price}
+							</div>
+							<button className="btn btn-accent">
+								Add to cart
+							</button>
 						</div>
-						
 					</div>
 				</div>
 
-				<RelatedProducts />		
-			
-					
-				
+				<RelatedProducts />
 			</div>
 		</div>
 	);
