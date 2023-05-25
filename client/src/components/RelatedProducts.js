1db74884efa50b00d2d9e53824553d0f31a15d33
Edyane <edyanealves@gmail.com>
2023-05-24 22:14:54 -0300

    Feat: construindo component RelatedProducts

--- client/src/components/RelatedProducts.js
+++ client/src/components/RelatedProducts.js
@@ -2,8 +2,9 @@ import React from 'react';
 import useFetch from '../hooks/useFetch';
 import ProductSlider from "./ProductSlider"
 
-const RelatedProducts = () => {
-  const { data } = useFetch();
+const RelatedProducts = ({ categoryTitle}) => {
+  const { data } = useFetch(`/products?populate=*filters[categories][title]=${categoryTitle}`);
+  console.log(data);
   return <div>RelatedProducts</div>;
 };
 
