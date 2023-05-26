c4cd00028699b51479169dfc48b39d1402af05be
Edyane <edyanealves@gmail.com>
2023-05-25 22:02:09 -0300

    Feat: estruturando página de produtos relacionados

--- client/src/components/RelatedProducts.js
+++ client/src/components/RelatedProducts.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import useFetch from '../hooks/useFetch';
-import ProductSlider from "./ProductSlider"
+import React from "react";
+import useFetch from "../hooks/useFetch";
+import ProductSlider from "./ProductSlider";
 
-const RelatedProducts = ({ categoryTitle}) => {
-  const { data } = useFetch(`/products?populate=*filters[categories][title]=${categoryTitle}`);
-  console.log(data);
-  return <div>RelatedProducts</div>;
+const RelatedProducts = ({ categoryTitle }) => {
+	const { data } = useFetch(
+		`/products?populate=*filters[categories][title]=${categoryTitle}`
+	);
+	return (
+    <div className="bg-pink-200 mb-16">
+      <div>
+        <h2>Related Products</h2>
+      </div>
+    </div>
+  )
 };
 
 export default RelatedProducts;
