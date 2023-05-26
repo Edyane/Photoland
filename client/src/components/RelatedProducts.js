5ca1a92831913502c31ca8ffecc84075e7e69299
Edyane <edyanealves@gmail.com>
2023-05-25 22:03:45 -0300

    Style: ajustando código

--- client/src/components/RelatedProducts.js
+++ client/src/components/RelatedProducts.js
@@ -7,12 +7,12 @@ const RelatedProducts = ({ categoryTitle }) => {
 		`/products?populate=*filters[categories][title]=${categoryTitle}`
 	);
 	return (
-    <div className="bg-pink-200 mb-16">
-      <div className="container mx-auto">
-        <h2>Related Products</h2>
-      </div>
-    </div>
-  )
+		<div className="bg-pink-200 mb-16">
+			<div className="container mx-auto">
+				<h2>Related Products</h2>
+			</div>
+		</div>
+	);
 };
 
 export default RelatedProducts;
