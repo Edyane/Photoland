ce5f084a1fec0aec1cd8f60c1ad03e586f6fb315
Edyane <edyanealves@gmail.com>
2023-06-01 22:40:07 -0300

    Style: ajustando código

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Cart = () => {
-  return <div>Cart</div>;
+	return (
+		<div classname="w-full h-full px-4 text-white">
+			<div>
+				<div>Close Icon</div>
+			</div>
+		</div>
+	);
 };
 
 export default Cart;
