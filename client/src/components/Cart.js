b84181d8289de9e9169e22e63dc1d3711df407fe
Edyane <edyanealves@gmail.com>
2023-06-01 22:44:09 -0300

    Feat: construindo página cart

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,10 +1,15 @@
 import React from "react";
 
+//icons 
+import { IoClose } from "react-icons/io5";
+
 const Cart = () => {
 	return (
 		<div classname="w-full h-full px-4 text-white">
 			<div>
-				<div>Close Icon</div>
+				<div className="text-4x1 w-20 h-[98px] flex justify-start items-center cursor-pointer">
+					<IoClose />
+				</div>
 			</div>
 		</div>
 	);
