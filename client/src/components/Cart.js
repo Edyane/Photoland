2fd13a49f307de5a34643b22ee9989107568b00a
Edyane <edyanealves@gmail.com>
2023-06-01 22:46:38 -0300

    Feat: construindo página cart

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 
 //icons 
 import { IoClose } from "react-icons/io5";
 
+//context
+import { CartContext } from "../context/CartContext";
+
 const Cart = () => {
+	const { setIsOpen } = useContext(CartContext)
 	return (
 		<div classname="w-full h-full px-4 text-white">
 			<div>
-				<div className="text-4x1 w-20 h-[98px] flex justify-start items-center cursor-pointer">
+				<div onClick={()=> setIsOpen(false)} className="text-4x1 w-20 h-[98px] flex justify-start items-center cursor-pointer">
 					<IoClose />
 				</div>
 			</div>
