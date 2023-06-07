bca87115cd528ce1ba6b22a61ac8382af7675602
Edyane <edyanealves@gmail.com>
2023-06-06 21:44:13 -0300

    fix: corrigindo bugs

--- client/src/components/RelatedProducts.js
+++ client/src/components/RelatedProducts.js
@@ -4,13 +4,13 @@ import ProductSlider from "./ProductSlider";
 
 const RelatedProducts = ({ categoryTitle }) => {
 	const { data } = useFetch(
-		`/products?populate=*filters[categories][title]=${categoryTitle}`
+		`/products?populate=*&filters[categories][title]=${categoryTitle}`
 	);
 	return (
 		<div className="mb-16">
 			<div className="container mx-auto">
 				<h2 className="h2 mb-6 text-center xl:text-left">Related Products</h2>
-				<RelatedProducts data={data} />
+				<ProductSlider data={data} />
 			</div>
 		</div>
 	);
