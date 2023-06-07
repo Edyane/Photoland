da03e23b043f1330be940d11dbf40bcaf77e2dc2
Edyane <edyanealves@gmail.com>
2023-06-06 21:43:42 -0300

    style: ajustando código

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,26 +1,26 @@
-import { useState, useEffect } from 'react';
-import { request } from '../request';
+import { useState, useEffect } from "react";
+import { request } from "../request";
 
 const useFetch = (url) => {
-  const [data, setData] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+	const [data, setData] = useState(null);
+	const [isLoading, setIsLoading] = useState(false);
+	const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      setIsLoading(true);
-      try {
-        const res = await request.get(url);
-        setData(res.data.data);
-      } catch (error) {
-        setError(error);
-      }
-      setIsLoading(false);
-    };
-    fetchData();
-  }, [url]);
+	useEffect(() => {
+		const fetchData = async () => {
+			setIsLoading(true);
+			try {
+				const res = await request.get(url);
+				setData(res.data.data);
+			} catch (error) {
+				setError(error);
+			}
+			setIsLoading(false);
+		};
+		fetchData();
+	}, [url]);
 
-  return { data, isLoading, error };
+	return { data, isLoading, error };
 };
 
 export default useFetch;
