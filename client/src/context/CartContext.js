9b38b73c2d2428cbb6d6cffd8d5fd4251c542452
Edyane <edyanealves@gmail.com>
2023-06-06 21:44:33 -0300

    fix: ajustando arquivo

--- client/src/context/CartContext.js
+++ client/src/context/CartContext.js
@@ -4,22 +4,7 @@ export const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
 	const [isOpen, setIsOpen] = useState(false);
-	const [itemsAmount, setItemsAmount] = useState(0);
-	const [amount, setAmount] = useState(0);
-	const [total, setTotal] = useState(0);
-
-	//add to cart 
-	// const addToCart = (item, id)=> {
-	// 	const itemId = parseInt(id);
-	// 	const newItem = {...item[0], amount: 1};
-	// 	setCart([...CartContext, newItem]);
-
-	// 	const cartItem = cart.find(item => {
-	// 		return item.id === itemId;
-	// 	})
-	// 	console.log(cart)
-	// };
-
+	
 	return (
 		<CartContext.Provider value={{ isOpen, setIsOpen }}>
 			{children}
