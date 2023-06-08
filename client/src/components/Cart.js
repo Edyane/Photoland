82b85fbf378aab6ffc78cc92f9477d4032e858d7
Edyane <edyanealves@gmail.com>
2023-06-07 21:15:56 -0300

    feat: ajustando cart e cartContext

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -9,7 +9,7 @@ import { CartContext } from "../context/CartContext";
 const Cart = () => {
 	const { setIsOpen } = useContext(CartContext)
 	return (
-		<div classname="w-full h-full px-4 text-white">
+		<div className="w-full h-full px-4 text-white">
 			<div>
 				<div onClick={()=> setIsOpen(false)} className="text-4x1 w-20 h-[98px] flex justify-start items-center cursor-pointer">
 					<IoClose />
--- client/src/context/CartContext.js
+++ client/src/context/CartContext.js
@@ -4,6 +4,16 @@ export const CartContext = createContext();
 
 const CartProvider = ({ children }) => {
 	const [isOpen, setIsOpen] = useState(false);
+	const [cart, setCart] = useState([]);
+	const [itemsAmount, setItemsAmount] = useState(0);
+	const [amount, setAmount] = useState(0);
+	const [total, setTotal] = useState(0);
+
+	//Add to cart 
+	const addToCart = (item, id) => {
+		console.log('added to cart');
+	}
+
 	
 	return (
 		<CartContext.Provider value={{ isOpen, setIsOpen }}>
