3533945a57f7d875612eaf92597ff5a2f00af99e
Edyane <edyanealves@gmail.com>
2023-06-08 10:54:11 -0300

    fix: ajustando cart

--- client/src/components/Cart.js
+++ client/src/components/Cart.js
@@ -6,13 +6,21 @@ import { IoClose } from "react-icons/io5";
 //context
 import { CartContext } from "../context/CartContext";
 
+//cart item
+import CartItem from "../components/CartItem";
+
 const Cart = () => {
-	const { setIsOpen } = useContext(CartContext)
+	const { setIsOpen, cart } = useContext(CartContext);
 	return (
 		<div className="w-full h-full px-4 text-white">
 			<div>
 				<div onClick={()=> setIsOpen(false)} className="text-4x1 w-20 h-[98px] flex justify-start items-center cursor-pointer">
 					<IoClose />
+				</div>
+				<div>
+					{cart.map((item) => {
+						return <CartItem item={item} key={item.id} />;
+					})}
 				</div>
 			</div>
 		</div>
