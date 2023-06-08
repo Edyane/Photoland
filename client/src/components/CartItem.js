e60d417b5f4aa61917293bd08b9a496ca5a5214e
Edyane <edyanealves@gmail.com>
2023-06-08 10:54:34 -0300

    feat: construindo cartItem

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const CartItem = () => {
-	return <div>CartItem</div>;
+const CartItem = ({ item }) => {
+	return (
+		<div className="flex">
+			<Link to={`product/${item.id}`} className="w-[70px] h-[70px]">
+				<img
+					src={`http://localhost:1337${item.attributes.image.data.attributes.url}`}
+					alt=""
+				/>
+			</Link>
+			<div>
+				{/* title and remove icon */}
+			</div>
+		</div>
+	);
 };
 
 export default CartItem;
