223c57b437374b7dcaf03dfc2bb228f66fba2e10
Edyane <edyanealves@gmail.com>
2023-06-09 21:27:15 -0300

    feat: construindo cartItem

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -1,17 +1,39 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { IoClose } from "react-icons/io5";
+import Qty from "../components/Qty";
 
 const CartItem = ({ item }) => {
 	return (
-		<div className="flex">
+		<div className="flex gap-x-8">
 			<Link to={`product/${item.id}`} className="w-[70px] h-[70px]">
 				<img
 					src={`http://localhost:1337${item.attributes.image.data.attributes.url}`}
 					alt=""
 				/>
 			</Link>
-			<div>
+			<div className="flex-1">
 				{/* title and remove icon */}
+				<div className="flex gap-x-4">
+					<Link to={`product/${item.id}`}>
+						{item.attributes.title}
+					</Link>
+					<div className="cursor-pointer text-[24px] hover:text-accent translation-all">
+						<IoClose />
+					</div>
+					{/* quantity */}
+				</div>
+
+				<div>
+					<div>
+						<Qty item={item} />
+					</div>
+					<div>$ {item.attributes.price * item.amount}</div>
+				</div>
+				{/* price*/}
+				<div className="text-accent">
+					<span>$ {item.attributes.price} per piece</span>
+				</div>
 			</div>
 		</div>
 	);
