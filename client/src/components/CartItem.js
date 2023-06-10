bcf442bc9c1fa1b26f93bdacd0a53164d5340ec2
Edyane <edyanealves@gmail.com>
2023-06-09 21:51:30 -0300

    feat: ajustando parâmetros

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { IoClose } from "react-icons/io5";
 import Qty from "../components/Qty";
+import { CartContext } from "../context/CartContext";
 
 const CartItem = ({ item }) => {
+	const { removeFromCart } = useContext(CartContext)
 	return (
 		<div className="flex gap-x-8">
 			<Link to={`product/${item.id}`} className="w-[70px] h-[70px]">
@@ -14,17 +16,17 @@ const CartItem = ({ item }) => {
 			</Link>
 			<div className="flex-1">
 				{/* title and remove icon */}
-				<div className="flex gap-x-4 mb-3 bg-pink-50/10">
+				<div className="flex gap-x-4 mb-3">
 					<Link to={`product/${item.id}`}>
 						{item.attributes.title}
 					</Link>
-					<div className="cursor-pointer text-[24px] hover:text-accent translation-all">
+					<div onClick={() => removeFromCart(item.id)}className="cursor-pointer text-[24px] hover:text-accent translation-all">
 						<IoClose />
 					</div>
 					{/* quantity */}
 				</div>
 
-				<div className="bg-yellow-200/20 flex items-center gap-x-12">
+				<div className="flex items-center gap-x-12">
 					<div className="flex gap-x-4 mb-2">
 						<Qty item={item} />
 						<div className="text-accent text-xl">$ {item.attributes.price * item.amount}</div>
