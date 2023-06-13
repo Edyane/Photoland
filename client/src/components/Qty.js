afaf7096b7e3ab6ae2be4cae66db6f91ff1b1b92
Edyane <edyanealves@gmail.com>
2023-06-12 21:45:13 -0300

    feat: construindo Qty

--- client/src/components/Qty.js
+++ client/src/components/Qty.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-const Qty = () => {
-	return <div>Qty</div>;
+//context
+import { CartContext } from "../context/CartContext";
+
+const Qty = ({ item }) => {
+	console.log(item);
+	return <div>
+		{item.amount > 10 ? <select>Select </select> : <input type="text"/>}
+	</div>;
 };
 
 export default Qty;
