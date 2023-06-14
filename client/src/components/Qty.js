3930dfa60faf46cf3b330e2d82fc438b80ff5269
Edyane <edyanealves@gmail.com>
2023-06-13 22:11:38 -0300

    feat: adicionando classe

--- client/src/components/Qty.js
+++ client/src/components/Qty.js
@@ -6,7 +6,8 @@ import { CartContext } from "../context/CartContext";
 const Qty = ({ item }) => {
 	console.log(item);
 	return <div>
-		{item.amount > 10 ? <select>Select </select> : <input className="text-primary placeholder:text-primary h-12 rounded-md p-4 w-[120px]" type="text" placeholder={`${item.amount}`}/>}
+		{item.amount > 10 ? <select>Select </select> : <input className="text-primary placeholder:text-primary h-12
+	 rounded-md p-4 w-[120px] outline-accent" type="text" placeholder={`${item.amount}`}/>}
 	</div>;
 };
 
