1651bac7cf912ac0c5c9d47d7e504cf0d228111c
Edyane <edyanealves@gmail.com>
2023-06-17 22:23:18 -0300

    feat: adicionando options

--- client/src/components/Qty.js
+++ client/src/components/Qty.js
@@ -1,15 +1,24 @@
 import React from "react";
 
 //context
-// import { CartContext } from "../context/CartContext";
+import { CartContext } from "../context/CartContext";
 
 const Qty = ({ item }) => {
 	console.log(item);
 	return (
 		<div>
 			{item.amount > 10 ? (
-				<select value={item.amount} className="p-2">
-					Select
+				<select value={item.amount} className="p-2 rounded-lg w-[100px] h-12 outline-none text-primary">
+					<option value="1">1</option>
+					<option value="2">2</option>
+					<option value="3">3</option>
+					<option value="4">4</option>
+					<option value="5">5</option>
+					<option value="6">6</option>
+					<option value="7">7</option>
+					<option value="8">8</option>
+					<option value="9">9</option>
+					<option value="10">10</option>
 				</select>
 			) : (
 				<input
