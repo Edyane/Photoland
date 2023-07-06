4b7b3fb2385905d9c34e1fe804001b332ade7524
Edyane <edyanealves@gmail.com>
2023-07-05 22:04:43 -0300

    feat: construindo footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { FaYoutube, FaInstagram, FaTwitter, FaFacebook } from "react-icons/fa";
 
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<footer className="pt-16" bg-primary>
+			<div className="container mx-auto">
+				<div>
+					<h2>Subscribe to our newsletter</h2>
+				</div>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
